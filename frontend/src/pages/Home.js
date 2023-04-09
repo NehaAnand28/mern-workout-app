08f795820777ddafc86e5a73061abb2de9284e59
Neha Anand <neha280103@gmail.com>
2023-04-09 18:18:27 +0530

    Added react frontend and fetching data

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+//components
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+const Home = () => {
+    const [workouts, setworkouts] = useState(null)
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+
+            if (response.ok) {
+                setworkouts(json)
+            }
+        }
+        fetchWorkouts()
+    }, [])
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => {
+                   return (<WorkoutDetails key={workout._id} workout={workout}/>)
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Home
