9db3cb6c221625528502f2f8ec7ec304598ca0ab
Neha Anand <neha280103@gmail.com>
2023-04-09 23:33:43 +0530

    adding patch method

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -4,7 +4,7 @@ import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const WorkoutDetails = ({ workout }) => {
     const {dispatch} = useWorkoutsContext()
-    const handleClick = async() => {
+    const handleDelete = async() => {
         const response = await fetch('/api/workouts/' + workout._id,{
             method : 'DELETE'
         })
@@ -15,13 +15,28 @@ const WorkoutDetails = ({ workout }) => {
         }
 
     }
+    // const handleUpdate = async () => {
+    //     const response = await fetch('/api/workouts/' + workout._id, {
+    //         method: 'PATCH'
+    //     })
+    //     const json = await response.json()
+
+    //     if (response.ok) {
+    //         dispatch({ type: 'UPDATE_WORKOUT', payload: json })
+    //     }
+
+    // }
     return (
         <div className='workout-details'>
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps : </strong>{workout.reps}</p>
             <p>{formatDistanceToNow(new Date(workout.createdAt),{addSuffix:true})}</p>
-            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+            <p>
+                <span className="material-symbols-outlined" onClick={handleDelete}>delete</span>
+                {/* <span className="material-symbols-outlined" onClick={handleUpdate}>edit</span> */}
+            </p>
+            
         </div>
     )
 }
