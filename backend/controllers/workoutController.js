908dbc529e310a9aad8e59ed3800924c18398875
Neha Anand <neha280103@gmail.com>
2023-04-09 23:02:46 +0530

    Error handling empty fields

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 //GET all workouts
 const getAllWorkouts = async (req, res) => {
     //gets all workout in array
-    const workouts = await Workout.find({}).sort({createdAt: -1})//decending order -> newest at top
+    const workouts = await Workout.find({}).sort({ createdAt: -1 })//decending order -> newest at top
     res.status(200).json(workouts)
 }
 
@@ -12,21 +12,34 @@ const getAllWorkouts = async (req, res) => {
 const getWorkout = async (req, res) => {
     const { id } = req.params
     //if not a valid object id, return 404 
-    if(!mongoose.Types.ObjectId.isValid(id)){
-        return res.status(404).json({error: 'No such workout'})
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such workout' })
     }
     const workout = await Workout.findById(id)
 
-    if(!workout) {
-        return res.status(404).json({error:'No such workout'})
+    if (!workout) {
+        return res.status(404).json({ error: 'No such workout' })
     }
 
     res.status(200).json(workout)
 }
 
 //POST a new workout
-const createWorkout = async(req,res) => {
+const createWorkout = async (req, res) => {
     const { title, load, reps } = req.body
+    let emptyFields = []
+    if (!title) {
+        emptyFields.push('title')
+    }
+    if (!load) {
+        emptyFields.push('load')
+    }
+    if (!reps) {
+        emptyFields.push('reps')
+    }
+    if(emptyFields.length > 0){
+        return res.status(400).json({error:'Please fill in all required fields',emptyFields})
+    }
     //add doc to db 
     try {
         //.create() is asynchronous -> use async & await -> response object with id stored in workout
@@ -38,14 +51,14 @@ const createWorkout = async(req,res) => {
 }
 
 //DELETE a workout
-const deleteWorkout = async(req, res) => { 
+const deleteWorkout = async (req, res) => {
     const { id } = req.params
     //if not a valid object id, return 404 
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({ error: 'No such workout' })
     }
 
-    const workout = await Workout.findOneAndDelete({_id: id})
+    const workout = await Workout.findOneAndDelete({ _id: id })
     if (!workout) {
         return res.status(404).json({ error: 'No such workout' })
     }
@@ -61,7 +74,7 @@ const updateWorkout = async (req, res) => {
         return res.status(404).json({ error: 'No such workout' })
     }
 
-    const workout = await Workout.findOneAndUpdate({ _id: id },{
+    const workout = await Workout.findOneAndUpdate({ _id: id }, {
         ...req.body
     })
     if (!workout) {
--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
+    const [emptyFields,setEmptyFields] = useState([])
     const handleSubmit = async(e) => {
         e.preventDefault()//to avoid default refresh action
         const workout = {title,load,reps}
@@ -21,12 +22,14 @@ const WorkoutForm = () => {
 
         if(!response.ok){
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if(response.ok){
             setTitle('')
             setLoad('')
             setReps('')
             setError(null)
+            setEmptyFields([])
             console.log('new workout added successfully',json)
             dispatch({type : 'CREATE_WORKOUT',payload:json})
         }
@@ -39,6 +42,7 @@ const WorkoutForm = () => {
                 type='text'
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : '' }
             />
 
             <label>Load (in kg): </label>
@@ -46,6 +50,7 @@ const WorkoutForm = () => {
                 type='number'
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps : </label>
@@ -53,6 +58,7 @@ const WorkoutForm = () => {
                 type='number'
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
             <button>Add Workout</button>
             {error && <div className='error'>{error}</div>}
